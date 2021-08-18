f61a77582ec17e4a4944ce2b0ab90bedc788f5da
My Name <hehuan@89trillion.com>
2021-08-18 12:39:38 +0800

    修改商品预制体类和其控制类的方法结构和逻辑

--- Assets/Script/Controller/DailySelection/CommodityController.cs
+++ Assets/Script/Controller/DailySelection/CommodityController.cs
@@ -42,10 +42,17 @@ public class CommodityController : MonoBehaviour
         {
 	        //生成商品
 	        CommodityPrefab commodityobj = Instantiate(commodityPrefab, commodityPanel.transform);
+	        commodityobj.commodity = commodity;
             count++;
 
-            //初始化商品子物体的各信息
-            InitializeInfo(commodity, commodityobj);
+            //初始化商品数量
+            commodityobj.ModifyCommodityNum(commodityobj.commodity.num);
+            
+            //初始化商品图片
+            commodityobj.ModifyCardBackground();
+            
+            //初始化商品按钮花费图片显示
+            commodityobj.ModifyButtonShow();
 
             //商品购买按钮激活，购买成功按钮关闭，购买按钮绑定购买事件
 			commodityobj.ModifyButtonState(0);
@@ -59,76 +66,15 @@ public class CommodityController : MonoBehaviour
 		  	count++;
 	    }
     }
-    
-    //初始化商品子物体的各信息
-    private void InitializeInfo(Commodity commodity, CommodityPrefab commodityObject)
-    {
-	    StringBuilder stringBuilder = new StringBuilder();
-	    int number = 0;
-	    
-	    //利用随机数判断第一个格子是金币还是钻石
-	    System.Random random= new System.Random();
-	    number = random.Next(1,3);
 
-	    //初始化商品数量
-	    commodityObject.ModifyCommodityNum(commodity.num);
-		
-	    //卡牌类型的图片初始化,使用紫色背景,不同类型的图片使用不同的卡牌
-	    if (commodity.type == 3) 
-	    {
-		    commodityObject.nameText.text = "卡牌";
-		    commodityObject.cardBackground.sprite = Resources.Load<Sprite>("Images/card_purple_bg");
-
-		    stringBuilder.Append("Images/");
-		    stringBuilder.Append(commodity.subType);
-		    commodityObject.cardImage.sprite = Resources.Load<Sprite>(stringBuilder.ToString());
-		    stringBuilder.Clear();
-	    } 
-	    //随机数判断显示是金币还是钻石，为1显示为金币，为2显示为钻石
-	    else
-	    {
-		    commodityObject.costNum = 0;
-		    commodityObject.costNumText.text = "0";
-		    commodityObject.cardBackground.sprite = Resources.Load<Sprite>("Images/card_coin_gem");
-		    if (number == 1) 
-		    {
-			    commodityObject.nameText.text = "金币";
-			    commodityObject.cardImage.sprite = Resources.Load<Sprite>("Images/coin_1");
-			    commodityObject.buyImage.sprite = Resources.Load<Sprite>("Images/coin");
-		    }
-		    else 
-		    { 
-			    commodityObject.nameText.text = "钻石"; 
-			    commodityObject.cardImage.sprite = Resources.Load<Sprite>("Images/diamond_2"); 
-			    commodityObject.buyImage.sprite = Resources.Load<Sprite>("Images/Gem");
-		    }
-	    }
-		
-	    //初始化商品按钮上花费金币数和金币图片显示
-	    if (commodity.costGold > 0) 
-	    {
-		    commodityObject.costNum = commodity.costGold;
-		    commodityObject.costNumText.text = commodity.costGold.ToString();
-		    commodityObject.buyImage.sprite = Resources.Load<Sprite>("Images/coin");
-	    } 
-	    //初始化商品按钮上花费钻石数和钻石图片显示
-	    else if (commodity.costGem > 0) 
-	    {
-		    commodityObject.costNum = commodity.costGem;
-		    commodityObject.costNumText.text = commodity.costGem.ToString();
-		    commodityObject.buyImage.sprite = Resources.Load<Sprite>("Images/Gem");
-	    } 
-         
-	} //InitializeInfo方法结束
-    
     //购买商品
     private void Buy(CommodityPrefab commodityObject)
     {
 	    //需花费的金币小于等于总金币且商品数量大于0，可以进行购买
-	    if (commodityObject.costNum <= playerInfoController.totalCoin && commodityObject.num >= 0)
+	    if (commodityObject.commodity.costGold <= playerInfoController.totalCoin && commodityObject.commodity.num >= 0)
 	    {
 		    //修改总金币显示
-		    playerInfoController.ModifyCoin(-commodityObject.costNum);
+		    playerInfoController.ModifyCoin(-commodityObject.commodity.costGold);
         
 		    //修改商品数量显示
 		    commodityObject.ModifyCommodityNum(-1);
--- Assets/Script/View/CommodityPrefab.cs
+++ Assets/Script/View/CommodityPrefab.cs
@@ -21,22 +21,78 @@ public class CommodityPrefab : MonoBehaviour
     public Text costNumText; //商品单价文字
     public GameObject successBtnObj; //已购买按钮对象
     
-    [HideInInspector]
-    public int num; //商品数量
-    [HideInInspector]
-    public int costNum; //商品单价
+    public Commodity commodity; //商品数据
+    
+    //修改卡牌图片或者钻石金币图片
+    public void ModifyCardBackground()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        int number = 0;
+	    
+        //利用随机数判断第一个格子是金币还是钻石
+        System.Random random= new System.Random();
+        number = random.Next(1,3);
+        
+        //卡牌类型的图片初始化,使用紫色背景,不同类型的图片使用不同的卡牌
+        if (commodity.type == 3) 
+        {
+            nameText.text = "卡牌";
+            cardBackground.sprite = Resources.Load<Sprite>("Images/card_purple_bg");
+
+            stringBuilder.Append("Images/");
+            stringBuilder.Append(commodity.subType);
+            cardImage.sprite = Resources.Load<Sprite>(stringBuilder.ToString());
+            stringBuilder.Clear();
+        } 
+        //随机数判断显示是金币还是钻石，为1显示为金币，为2显示为钻石
+        else
+        {
+            commodity.costGold = 0;
+            costNumText.text = "0";
+            cardBackground.sprite = Resources.Load<Sprite>("Images/card_coin_gem");
+            if (number == 1) 
+            {
+                nameText.text = "金币";
+                cardImage.sprite = Resources.Load<Sprite>("Images/coin_1");
+                buyImage.sprite = Resources.Load<Sprite>("Images/coin");
+            }
+            else 
+            { 
+                nameText.text = "钻石"; 
+                cardImage.sprite = Resources.Load<Sprite>("Images/diamond_2"); 
+                buyImage.sprite = Resources.Load<Sprite>("Images/Gem");
+            }
+        }
+    }
 
     //修改商品数量及其显示文字
     public void ModifyCommodityNum(int addNum)
     {
         StringBuilder stringBuilder = new StringBuilder();
-        num += addNum;
+        commodity.num += addNum;
         stringBuilder.Append("x");
-        stringBuilder.Append(num);
+        stringBuilder.Append(commodity.num);
         numText.text = stringBuilder.ToString();
     }
     
-    //修改商品下的购买按钮显示，state为0时，为购买按钮，state为1时，为已购买按钮
+    //修改商品按钮上花费图片显示
+    public void ModifyButtonShow()
+    {
+        //初始化商品按钮上花费金币数和金币图片显示
+        if (commodity.costGold > 0) 
+        {
+            costNumText.text = commodity.costGold.ToString();
+            buyImage.sprite = Resources.Load<Sprite>("Images/coin");
+        } 
+        //初始化商品按钮上花费钻石数和钻石图片显示
+        else if (commodity.costGem > 0) 
+        {
+            costNumText.text = commodity.costGem.ToString();
+            buyImage.sprite = Resources.Load<Sprite>("Images/Gem");
+        } 
+    }
+    
+    //修改商品下的购买按钮，state为0时，为购买按钮，state为1时，为已购买按钮
     public void ModifyButtonState(int state)
     {
         if (state == 0)
