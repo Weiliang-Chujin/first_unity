e66195e0831d9e35e04c7234b72c5378ab434275
My Name <hehuan@89trillion.com>
2021-08-18 12:46:31 +0800

    改了商品控制类的注释

--- Assets/Script/Controller/DailySelection/CommodityController.cs
+++ Assets/Script/Controller/DailySelection/CommodityController.cs
@@ -10,9 +10,9 @@ using UnityEngine.UI;
  */
 public class CommodityController : MonoBehaviour
 {
+	public PlayerInfoController playerInfoController; //玩家信息控制类
 	public GameObject dailyPanel; //每日精选标题容器
     public GameObject commodityPanel; //商品容器
-    public PlayerInfoController playerInfoController; //玩家信息控制类
 
     public GameObject dailyTitlePrefab; //每日精选标题预制体
     public CommodityPrefab commodityPrefab; //商品预制体
